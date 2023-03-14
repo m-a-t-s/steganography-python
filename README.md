682f36e018f6cb29bcf70e0d8e35f4f185157fd2
Mathew Sayed <mathew.sayed@gmail.com>
2023-03-14 10:20:34 +1000

    Updated
    Working code

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ Install the necessary dependencies by running pip install argparse pillow in you
 Open a terminal or command prompt and navigate to the directory where the steganography.py file is saved.
 To encode a message in an image, run the following command:
 
-`python steganography.py encode input_image.png -m "Your secret message" -o output_image.png`
+`python steganography.py`
 
-This will encode the message "Your secret message" into the image input_image.png and save the result to output_image.png. If you don't provide an output file path, the script will save the encoded image to a file named encoded.png.
+This will encode the message "Your secret message" into the image and save the result to output image.
 
 To decode a message from an image, run the following command:
 
-`python steganography.py decode input_image.png`
+`python steganography.py`
 
-This will decode the message from the image input_image.png and print it to the console.
+This will decode the message from the image input and print it to the console.
 
 Note that the input image must be in PNG format. The script uses the Pillow library to load and manipulate the images. Also, keep in mind that the message you want to encode should not be too long, as the maximum amount of information that can be embedded in an image depends on the image's dimensions.
--- steganography.py
+++ steganography.py
@@ -4,132 +4,139 @@
 # pixels of image and modify it
 from PIL import Image
 
-# Convert encoding data into 8-bit binary
-# form using ASCII value of characters
-def genData(data):
+def gen_data(data):
+    """
+    Convert encoding data into 8-bit binary form using ASCII value of characters.
+    :param data: str
+    :return: list
+    """
+    newd = []
+    for i in data:
+        newd.append(format(ord(i), '08b'))
+    return newd
 
-		# list of binary codes
-		# of given data
-		newd = []
 
-		for i in data:
-			newd.append(format(ord(i), '08b'))
-		return newd
+def modify_pixels(pix, data):
+    """
+    Modify pixels according to the 8-bit binary data and yield pixel values.
+    :param pix: tuple
+    :param data: str
+    :return: tuple
+    """
+    datalist = gen_data(data)
+    lendata = len(datalist)
+    imdata = iter(pix)
+    for i in range(lendata):
+        # Extracting 3 pixels at a time
+        pix = [value for value in imdata.__next__()[:3] + imdata.__next__()[:3] + imdata.__next__()[:3]]
+        # Pixel value should be made odd for 1 and even for 0
+        for j in range(0, 8):
+            if datalist[i][j] == '0' and pix[j] % 2 != 0:
+                pix[j] -= 1
+            elif datalist[i][j] == '1' and pix[j] % 2 == 0:
+                if pix[j] != 0:
+                    pix[j] -= 1
+                else:
+                    pix[j] += 1
+        # Eighth pixel of every set tells whether to stop or read further. 0 means keep reading; 1 means the message is over.
+        if i == lendata - 1:
+            if pix[-1] % 2 == 0:
+                if pix[-1] != 0:
+                    pix[-1] -= 1
+                else:
+                    pix[-1] += 1
+        else:
+            if pix[-1] % 2 != 0:
+                pix[-1] -= 1
+        pix = tuple(pix)
+        yield pix[0:3]
+        yield pix[3:6]
+        yield pix[6:9]
 
-# Pixels are modified according to the
-# 8-bit binary data and finally returned
-def modPix(pix, data):
 
-	datalist = genData(data)
-	lendata = len(datalist)
-	imdata = iter(pix)
-
-	for i in range(lendata):
-
-		# Extracting 3 pixels at a time
-		pix = [value for value in imdata.__next__()[:3] +
-								imdata.__next__()[:3] +
-								imdata.__next__()[:3]]
-
-		# Pixel value should be made
-		# odd for 1 and even for 0
-		for j in range(0, 8):
-			if (datalist[i][j] == '0' and pix[j]% 2 != 0):
-				pix[j] -= 1
-
-			elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
-				if(pix[j] != 0):
-					pix[j] -= 1
-				else:
-					pix[j] += 1
-				# pix[j] -= 1
-
-		# Eighth pixel of every set tells
-		# whether to stop ot read further.
-		# 0 means keep reading; 1 means thec
-		# message is over.
-		if (i == lendata - 1):
-			if (pix[-1] % 2 == 0):
-				if(pix[-1] != 0):
-					pix[-1] -= 1
-				else:
-					pix[-1] += 1
-
-		else:
-			if (pix[-1] % 2 != 0):
-				pix[-1] -= 1
-
-		pix = tuple(pix)
-		yield pix[0:3]
-		yield pix[3:6]
-		yield pix[6:9]
-
-def encode_enc(newimg, data):
-	w = newimg.size[0]
-	(x, y) = (0, 0)
-
-	for pixel in modPix(newimg.getdata(), data):
-
-		# Putting modified pixels in the new image
-		newimg.putpixel((x, y), pixel)
-		if (x == w - 1):
-			x = 0
-			y += 1
-		else:
-			x += 1
+def encode_image(newimg, data):
+    """
+    Encode the data into the image by modifying the pixels.
+    :param newimg: PIL Image object
+    :param data: str
+    """
+    w = newimg.size[0]
+    (x, y) = (0, 0)
+    for pixel in modify_pixels(newimg.getdata(), data):
+        # Putting modified pixels in the new image
+        newimg.putpixel((x, y), pixel)
+        if x == w - 1:
+            x = 0
+            y += 1
+        else:
+            x += 1
 
 # Encode data into image
 def encode():
-	img = input("Enter image name(with extension) : ")
-	image = Image.open(img, 'r')
+    # Open the image file
+    img = Image.open(input("Enter image name(with extension) : "), 'r')
 
-	data = input("Enter data to be encoded : ")
-	if (len(data) == 0):
-		raise ValueError('Data is empty')
+    # Get the data to be encoded
+    data = input("Enter data to be encoded : ").strip()
+    if not data:
+        raise ValueError('Data is empty')
 
-	newimg = image.copy()
-	encode_enc(newimg, data)
+    # Make a copy of the image
+    newimg = img.copy()
 
-	new_img_name = input("Enter the name of new image(with extension) : ")
-	newimg.save(new_img_name, str(new_img_name.split(".")[1].upper()))
+    # Encode the data in the image
+    encode_image(newimg, data)
+
+    # Save the new image
+    new_img_name = input("Enter the name of new image(with extension) : ")
+    newimg.save(new_img_name, new_img_name.split(".")[-1].upper())
+
+    # Output message to indicate encoding success
+    print(f"The data has been encoded in the image: {new_img_name}")
+
 
 # Decode the data in the image
 def decode():
-	img = input("Enter image name(with extension) : ")
-	image = Image.open(img, 'r')
+    # Open the image file
+    img = Image.open(input("Enter image name(with extension) : "), 'r')
 
-	data = ''
-	imgdata = iter(image.getdata())
+    # Initialize variables
+    data = ''
+    imgdata = iter(img.getdata())
 
-	while (True):
-		pixels = [value for value in imgdata.__next__()[:3] +
-								imgdata.__next__()[:3] +
-								imgdata.__next__()[:3]]
+    while True:
+        # Get the next 3 pixels
+        pixels = [value for value in imgdata.__next__()[:3] +
+                  imgdata.__next__()[:3] +
+                  imgdata.__next__()[:3]]
 
-		# string of binary data
-		binstr = ''
+        # Convert the pixel values to binary
+        binstr = ''.join(['0' if i % 2 == 0 else '1' for i in pixels[:8]])
 
-		for i in pixels[:8]:
-			if (i % 2 == 0):
-				binstr += '0'
-			else:
-				binstr += '1'
+        # Convert the binary string to ASCII and add to data
+        data += chr(int(binstr, 2))
 
-		data += chr(int(binstr, 2))
-		if (pixels[-1] % 2 != 0):
-			return data
+        # Check if the last pixel is odd, which indicates the end of the message
+        if pixels[-1] % 2 != 0:
+            return data
 
-# Main Function
+
 def main():
-	a = int(input(":: Welcome to Steganography ::\n"
-						"1. Encode\n2. Decode\n"))
-	if (a == 1):
-		encode()
+    # Ask user for choice
+    choice = int(input(":: Welcome to Steganography ::\n"
+                       "1. Encode\n2. Decode\n"))
 
-	elif (a == 2):
-		print("Decoded Word : " + decode())
-	else:
-		raise Exception("Enter correct input")
+    # Check user choice
+    if choice == 1:
+        # If user wants to encode, call encode function
+        encode()
+    elif choice == 2:
+        # If user wants to decode, call decode function and print the decoded message
+        decoded_message = decode()
+        print(f"Decoded Word: {decoded_message}")
+    else:
+        # If user enters an invalid choice, raise an exception
+        raise ValueError("Enter correct input")
 
 # Driver Code
 if __name__ == '__main__' :
